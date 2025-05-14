6276d289b5040b2b15992834cc779725594006cf
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-05-14 00:46:04 -0300

    Total agregado

--- src/components/DetallesCarro.js
+++ src/components/DetallesCarro.js
@@ -13,9 +13,26 @@ const styles = {
     padding: 0,
     boxShadow: "0px 5px 8px rgba(0,0,0,0.3)",
   },
+  total: {
+    backgroundColor: "#FFFFFF",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "50px",
+    padding: "5px 10px",
+    borderBottom: "1px solid #E1E1E1",
+    fontWeight: "bold",
+  },
 };
 
 export default class DetallesCarro extends Component {
+  calcularTotal(carro) {
+    let total = carro.reduce(
+      (acum, producto) => acum + producto.precio * producto.cantidad,
+      0
+    );
+    return total;
+  }
   render() {
     const { carro } = this.props;
     return (
@@ -23,6 +40,7 @@ export default class DetallesCarro extends Component {
         {carro.map((producto, index) => (
           <Detalle producto={producto} key={index} />
         ))}
+        <li style={styles.total}>Total: ${this.calcularTotal(carro)}</li>
       </ul>
     );
   }
